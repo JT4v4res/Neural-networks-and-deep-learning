0a3a977acbddb67a47ed8e0152f9a50917743e5d
JT4v4res <jvst@ic.ufal.br>
2023-12-09 11:30:01 -0300

    Fixes in multilayer and new based implementation

--- multi-layer neural network/multi-layer neural network.py
+++ multi-layer neural network/multi-layer neural network.py
@@ -1,3 +1,7 @@
+# this code was made with the chapter 13 example in deep learning book
+# https://www.deeplearningbook.com.br/
+
+import random
 import numpy as np
 
 
@@ -5,50 +9,100 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-class NetworkLayer:
-    def __init__(self, layer_length=0, input_size=0):
-        self.neurons = np.zeros(layer_length)
-        self.weights = self.weight_initialize(input_size)
-
-    def weight_initialize(self, input_size):
-        weights = []
-
-        for i in range(0, len(self.neurons)):
-            weights.append(np.random.rand(input_size))
-
-        return np.array(weights)
-
-    def affine_function(self, neuron_inputs,  bias):
-
-        for i in range(0, len(neuron_inputs)):
-            for j in range(0, len(self.weights)):
-                self.neurons[j] = sigmoid(np.sum(np.dot(neuron_inputs[i], self.weights[j])) + bias)
-
-        return self.neurons
+def sigmoid_derivative(x):
+    return sigmoid(x) * (1 - sigmoid(x))
 
 
 class NeuralNetwork:
-    def __init__(self, input_len=0):
-        self.neuron_layers = []
-        self.total_layers = 0
-        self.bias = 0
-        self.loss_function = 0
-        self.y_pred = []
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 
-    def train(self, epoch, entry, correct):
-        for i in range(0, epoch):
-            feed_input = entry
-            self.y_pred = []
-            for z in range(0, len(self.neuron_layers)):
-                feed_input = self.neuron_layers[z].affine_function(np.asarray(feed_input), self.bias)
-                print(feed_input)
+    def feed_forward(self, a):
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a) + b)
+        return a
+
+    def stochastic_gradient_descent(self, train_data, epochs, mini_batch_size, eta, test_data=None):
+        train_data = list(train_data)
+        n = len(train_data)
+
+        if len(test_data) > 0:
+            test_data = list(test_data)
+            n_test = len(test_data)
+
+        for j in range(epochs):
+            random.shuffle(train_data)
+            mini_batches = [train_data[k:k + mini_batch_size] for k in range(0, n, mini_batch_size)]
+
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+
+            if len(test_data) > 0:
+                print("Epoch {} : {} / {}".format(j, self.evaluate(test_data), n_test))
+            else:
+                print("Epoch {} finalizada".format(j))
+
+    def update_mini_batch(self, mini_batch, eta):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backpropagation(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+
+        self.weights = [w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
+
+    def backpropagation(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        # Feedforward
+        activation = x
+
+        # Lista contendo as ativacoes por cada camada
+        activations = [x]
+
+        # Lista com os Z's de cada camada
+        zs = []
+
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+
+            activation = sigmoid(z)
+            activations.append(activation)
+
+        # Backward
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_derivative(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_derivative(z)
+            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
+
+        return (nabla_b, nabla_w)
+
+    def cost_derivative(self, output_activation, y):
+        return output_activation - y
+
+    def evaluate(self, test_data):
+
+        test_results = [(np.argmax(self.feed_forward(x)), y) for x, y in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
 
 
 inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 outputs = np.array([0, 1, 1, 0])
 
-neuralNetwork = NeuralNetwork(2)
-neuralNetwork.neuron_layers.append(NetworkLayer(3, 2))
-neuralNetwork.neuron_layers.append(NetworkLayer(1, 3))
+neuralNetwork = NeuralNetwork([2, 3, 1])
 
-neuralNetwork.train(1, inputs, outputs)
+neuralNetwork.stochastic_gradient_descent(inputs, 1000, 2, 0.01, outputs)
