e8d4364ef3a89edeaa246531b3f5074039fe2df7
JT4v4res <joaotavarestavares@hotmail.com>
2024-02-18 12:44:12 -0300

    Alterações nas funções e na implementação da rede

--- multi-layer neural network/multi-layer neural network.py
+++ multi-layer neural network/multi-layer neural network.py
@@ -1,75 +1,164 @@
 import numpy as np
 
 
-def sigmoid_derivative(x):
-    return (1 - sigmoid(x)) * sigmoid(x)
+def sigmoid(x, derivative=False):
+    if derivative:
+        y = sigmoid(x)
+        return y * (1 - y)
 
-
-def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
+def linear(x, derivative=False):
+    if derivative:
+        return np.ones_like(x)
+    return x
+
+
+def tanh(x, derivative=False):
+    if derivative:
+        y = tanh(x)
+        return 1 - y ** 2
+    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
+
+def relu(x, derivative=False):
+    if derivative:
+        return np.where(x <= 0, 0, 1)
+
+    return np.maximum(0, x)
+
+
+def leaky_relu(x, derivative=False):
+    alpha=0.1
+    if derivative:
+        np.where(x <= 0, alpha, 1)
+    return np.where(x <= 0, alpha * x, x)
+
+
+def elu(x, derivative=False):
+    alpha = 1.0
+    if derivative:
+        y = elu(x)
+        return np.where(x <= 0, y + alpha, 1)
+    return np.where(x <= 0, alpha * (np.exp(x) - 1), x)
+
+
+def softmax(x, y_oh=None,derivative=False):
+    if derivative:
+        y_pred = softmax(x)
+        k = np.nonzero(y_pred * y_oh)
+        pk = y_pred[k]
+        y_pred[k] = pk * (1.0 - pk)
+        return y_pred
+    exp = np.exp(x)
+    return exp / np.sum(exp, axis=1, keepdims=True)
+
+
+def neg_log_likelihood(y_oh, y_pred, derivative=False):
+    k = np.nonzero(y_pred * y_oh)
+    pk = y_pred[k]
+    if derivative:
+        y_pred[k] = (-1.0 / pk)
+        return y_pred
+    return np.mean(-np.log(pk))
+
+
+def softmax_neg_log_likelihood(y_oh, y_pred, derivative=False):
+    y_softmax = softmax(y_pred)
+    if derivative:
+        return -(y_oh - y_softmax) / y_oh.shape[0]
+    return neg_log_likelihood(y_oh, y_softmax)
+
+
+def mean_absolute_error(y, y_pred, derivative=False):
+    if derivative:
+        return np.where(y_pred > y, 1, -1) / y.shape[0]
+    return np.mean(np.abs(y - y_pred))
+
+
+def mean_squared_error(y, y_pred, derivative=False):
+    if derivative:
+        return -(y - y_pred) / y.shape[0]
+    return 0.5 * np.mean((y - y_pred) ** 2)
+
+
+def binary_cross_entropy_error(y, y_pred, derivative=False):
+    if derivative:
+        return -(y - y_pred) / (y_pred * (1 - y_pred) * y.shape[0])
+    return -np.mean(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred))
+
+
+def sigmoid_cross_entropy(y, y_pred, derivative=False):
+    y_sigmoid = sigmoid(y_pred)
+    if derivative:
+        return -(y - y_sigmoid) / (y_sigmoid * (1 - y_sigmoid) * y.shape[0])
+    return -np.mean(y * np.log(y_sigmoid) + (1 - y) * np.log(1 - y_sigmoid))
+
+
 class NetworkLayer:
-    def __init__(self, layer_length=0, input_size=0):
-        self.neurons = np.zeros(layer_length)
-        self.weights = self.weight_initialize(input_size)
+    def __init__(self, input_shape, output_shape, activation=linear):
+        self.input = None
+        self.weights = np.random.randn(output_shape, input_shape)
+        self.biases = np.random.randn(1, output_shape)
+        self.activation = activation
 
-    def weight_initialize(self, input_size):
-        weights = []
-
-        for i in range(0, len(self.neurons)):
-            weights.append(np.random.rand(input_size))
-
-        return np.array(weights)
-
-    def affine_function(self, neuron_inputs,  bias):
-
-        for i in range(0, len(neuron_inputs)):
-            for j in range(0, len(self.weights)):
-                self.neurons[j] = sigmoid(np.sum(np.dot(neuron_inputs[i], self.weights[j])) + bias)
-
-        return self.neurons
+        self._activation_input, self._activation_output = None, None
+        self._dweights, self._dbiases = None, None
 
 
 class NeuralNetwork:
-    def __init__(self, input_len=0):
-        self.neuron_layers = []
-        self.total_layers = 0
-        self.bias = 0
-        self.loss_function = 0
-        self.y_pred = []
-        self.learning_rate = 0.1
+    def __init__(self, cost_func=mean_squared_error, learning_rate=1e-3):
+        self.layers = []
+        self.cost_func = cost_func
+        self.learning_rate = learning_rate
 
-    def train(self, epoch, entry):
-        for i in range(0, epoch):
-            feed_input = entry
-            self.y_pred = []
-            for z in range(0, len(self.neuron_layers)):
-                feed_input = self.neuron_layers[z].affine_function(np.asarray(feed_input), self.bias)
+    def fit(self, x_train, y_train, epochs=100, verbose=10):
+        for epoch in range(0, epochs + 1):
+            y_pred = self.__feedforward(x_train)
+            self.__backpropagation(y_train, y_pred)
 
-            print(feed_input)
+            if epoch % verbose == 0:
+                loss_train = self.cost_func(y_train, self.predict(x_train))
+                print(f'Epoch: {epoch}/{epochs}, Loss: {loss_train}')
 
-            delta_out = sigmoid_derivative(feed_input)
+    def predict(self, x):
+        return self.__feedforward(x)
 
-            last_deltas = dict()
+    def __feedforward(self, x):
+        self.layers[0].input = x
 
-            self.neuron_layers[-1].weights += (delta_out * self.learning_rate)
+        for layer, next_layer in zip(self.layers, self.layers[1:] + [NetworkLayer(0, 0)]):
+            y = np.dot(layer.input, layer.weights.T) + layer.biases
+            layer._activation_input = y
+            layer._activation_output = next_layer.input = layer.activation(y)
 
-            last_deltas[len(self.neuron_layers) - 1] = delta_out
+        return self.layers[-1]._activation_output
 
-            for z in range(len(self.neuron_layers) - 2, -1, -1):
-                for idw, w in enumerate(self.neuron_layers[z].weights):
-                    idx = 0
-                    last_deltas[z] = w * last_deltas[z + 1]
-                    self.neuron_layers[z].weights[idw] = w + self.learning_rate * last_deltas[z + 1][idx]
-                    idx += 1
+    def __backpropagation(self, y, y_pred):
+        last_delta = self.cost_func(y, y_pred, True)
+
+        for layer in reversed(self.layers):
+            dactivation = layer.activation(layer._activation_input, True) * last_delta
+            last_delta = np.dot(dactivation, layer.weights)
+            layer._dweights = np.dot(dactivation.T, layer.input)
+            layer._dbiases = 1.0 * dactivation.sum(axis=0, keepdims=True)
+
+        for layer in reversed(self.layers):
+            layer.weights = layer.weights - self.learning_rate * layer._dweights
+            layer.biases = layer.biases - self.learning_rate * layer._dbiases
 
 
 inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 outputs = np.array([0, 1, 1, 0])
 
-neuralNetwork = NeuralNetwork(2)
-neuralNetwork.neuron_layers.append(NetworkLayer(3, 2))
-neuralNetwork.neuron_layers.append(NetworkLayer(1, 3))
+input_dim, output_dim = inputs.shape[1], outputs.shape[0]
 
-neuralNetwork.train(100, inputs)
+neural_network = NeuralNetwork(cost_func=mean_squared_error, learning_rate=0.7)
+neural_network.layers.append(NetworkLayer(input_dim, 2, activation=sigmoid))
+neural_network.layers.append(NetworkLayer(2, output_dim, activation=sigmoid))
+
+neural_network.fit(inputs, outputs, 1000)
+
+print(neural_network.predict(inputs))
+
